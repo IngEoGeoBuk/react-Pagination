6cf4d381b4dc6f590b0d5783aa6ea68b2d9e7941
IngEoGeoBuk <59249899+IngEoGeoBuk@users.noreply.github.com>
2021-04-10 16:24:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,55 +47,76 @@ export default PaginatedContent;
 
 
 
-// import React, {useState, useEffect} from 'react'
-// import axios from 'axios'
+// import React, { useState, useEffect } from 'react'
+// import Axios from 'axios';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+// import Paper from '@material-ui/core/Paper';
+// import Typography from '@material-ui/core/Typography';
+// import Colors from '../../styles/Colors'
 // import ReactPaginate from 'react-paginate';
-// // import './App.css'
+// import './Home.css';
 
-// function App() {
-//   const [offset, setOffset] = useState(0);
-//   const [data, setData] = useState([]);
-//   const [perPage] = useState(10);
-//   const [pageCount, setPageCount] = useState(0)
+// const White = Colors.White;
+// const Indigo = Colors.Indigo;
 
-
-//   const getData = async() => {
-//       const res = await axios.get(`https://jsonplaceholder.typicode.com/photos`)
-//       const data = res.data;
-//                 const slice = data.slice(offset, offset + perPage)
-//                 const postData = slice.map((pd : any) => <div key={pd.id}>
-//                     <p>{pd.title}</p>
-//                     <img src={pd.thumbnailUrl} alt=""/>
-//                 </div>)
-//                 setData(postData)
-//                 setPageCount(Math.ceil(data.length / perPage))
-//   }
-//   const handlePageClick = (e: any) => {
-//     const selectedPage = e.selected;
-//     setOffset(selectedPage + 1)
-// };
-
-//  useEffect(() => {
-//    getData()
-//  }, [offset])
-
-//   return (
-//     <div className="App">
-//       {data}
-//        <ReactPaginate
-//                     previousLabel={"prev"}
-//                     nextLabel={"next"}
-//                     breakLabel={"..."}
-//                     breakClassName={"break-me"}
-//                     pageCount={pageCount}
-//                     marginPagesDisplayed={2}
-//                     pageRangeDisplayed={6}
-//                     onPageChange={handlePageClick}
-//                     containerClassName={"pagination"}
-//                     // subContainerClassName={"pages pagination"}
-//                     activeClassName={"active"}/>
-//     </div>
-//   );
+// interface postTypes {
+//     id?: number;
+//     title: string;
+//     contents: string;
 // }
 
-// export default App;
+// const Home = () => {
+//     const [postList, setPostList] = useState<postTypes[]>([]);
+//     useEffect(() => {
+//         Axios.get('http://localhost:5000/read')
+//         .then((res) => setPostList(res.data));
+//     }, [])
+
+//     const [pageNumber, setPageNumber] = useState(0);
+//     const postPerPage = 5;
+//     const pagesVisited = pageNumber * postPerPage;
+
+//     const displayPosts = postList
+//     .slice(pagesVisited, pagesVisited + postPerPage)
+//     .map((val : postTypes , key : number) => {
+//         return (
+//             <div>
+//                 <Paper elevation={2} style={{ padding: '10px' }} >
+//                     <Link to={`/team/${val.id}`} style={{ textDecoration: 'none' }}>
+//                         <h3 style={{ display: 'none' }}>{key}</h3>
+//                         <Typography style={{ color: Indigo }}>{val.title}</Typography>
+//                     </Link>
+//                 </Paper>
+//                 <br/>                    
+//             </div>
+//         );
+//     });
+
+//     const pageCount = Math.ceil(postList.length / postPerPage);
+
+//     const changePage = ({ selected } : any) => {
+//         setPageNumber(selected);
+//     };
+
+//     return (
+//         <div className="App">
+//             {displayPosts}
+//             <div className="pagination">
+//             <ReactPaginate
+//                 previousLabel={"prev"}
+//                 nextLabel={"next"}
+//                 pageCount={pageCount}
+//                 onPageChange={changePage}
+//                 breakLabel={"..."}
+//                 breakClassName={"break-me"}
+//                 marginPagesDisplayed={5}
+//                 pageRangeDisplayed={5}
+//                 containerClassName={"pagination"}
+//                 activeClassName={"active"}/>
+//             </div>
+//         </div>
+//     );
+
+// }
+
+// export default Home
